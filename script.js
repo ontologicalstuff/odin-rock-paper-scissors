db75e54dcd562431900c1641d74af2bba4a691fc
logicalmind <ilikelogic@skiff.com>
2022-08-22 13:03:53 -0700

    Declare function expression for one round of game

--- script.js
+++ script.js
@@ -12,3 +12,23 @@ let playerChoice = "Rock";
 playerChoice = playerChoice.toLowerCase();
 
 const computerChoice = getComputerChoice();
+
+let oneRound = function (player, computer) {
+    if (player === "rock" && computer === "scissors") {
+        return "You win! Rock beats scissors.";
+    } else if (player === "rock" && computer === "paper") {
+        return "You lose! Paper beats rock"; 
+    } else if (player === "scissors" && computer === "rock") {
+        return "You lose! Rock beats scissors!"
+    } else if (player === "scissors" && computer === "paper") {
+        return "You win! Scissors beats paper!";
+    } else if (player === "paper" && computer === "rock") {
+        return "You win! Paper beats rocks!";
+    } else if (player === "paper" && computer === "scissors") {
+        return "You lose! Scissors beats paper!";
+    } else {
+        return "It's a tie!"
+    }  
+}
+
+console.log(oneRound(playerChoice, computerChoice));
