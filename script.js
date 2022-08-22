911175c9a6d95bb182dbdd4a28b5c6c2cbe61758
logicalmind <ilikelogic@skiff.com>
2022-08-22 13:02:24 -0700

    Declare player and computer variables

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3)
     return choices[randomNumber];
 }
+
+/* Variables that represent computer and player choices */
+
+let playerChoice = "Rock";
+playerChoice = playerChoice.toLowerCase();
+
+const computerChoice = getComputerChoice();
