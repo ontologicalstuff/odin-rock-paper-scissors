62db3fdb1cdbe9e4403a04014796c4fd823a9e04
logicalmind <ilikelogic@skiff.com>
2022-08-22 14:39:34 -0700

    Add player and computer score variables to oneRound and declare for loop

--- script.js
+++ script.js
@@ -13,18 +13,28 @@ playerChoice = playerChoice.toLowerCase();
 
 const computerChoice = getComputerChoice();
 
+/* oneRound function */
+
 let oneRound = function (player, computer) {
+    let pScore;
+    let cScore;
     if (player === "rock" && computer === "scissors") {
-        return "You win! Rock beats scissors.";
+        pScore ++;
+        return "You win! Rock beats scissors!";
     } else if (player === "rock" && computer === "paper") {
-        return "You lose! Paper beats rock"; 
+        cScore ++;
+        return "You lose! Paper beats rock!"; 
     } else if (player === "scissors" && computer === "rock") {
+        cScore ++;
         return "You lose! Rock beats scissors!"
     } else if (player === "scissors" && computer === "paper") {
+        pScore ++;
         return "You win! Scissors beats paper!";
     } else if (player === "paper" && computer === "rock") {
+        pScore ++;
         return "You win! Paper beats rocks!";
     } else if (player === "paper" && computer === "scissors") {
+        cScore ++;
         return "You lose! Scissors beats paper!";
     } else {
         return "It's a tie!"
@@ -32,3 +42,7 @@ let oneRound = function (player, computer) {
 }
 
 console.log(oneRound(playerChoice, computerChoice));
+
+for (let i = 0; i < 5; i++) {
+    oneRound(playerChoice, computerChoice);
+}
